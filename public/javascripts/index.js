7bc44ec03516d94d6825c9c2a08bb473c12ffb3f
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-16 20:03:22 -0600

    add event listener for tr click in user_home

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,4 +4,12 @@ $(document).ready(() => {
   $(`.button-collapse`).sideNav()
   $(`.modal-trigger`).leanModal()
   $(`select`).material_select()
+
+  // event listener for click on tr,
+  // $(`tr`).click(() => {
+  //   const tick_id = $(this).children().text()
+  //
+  //   console.log(tick_id)
+  // })
+
 })
