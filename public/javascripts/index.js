94eda327bb1a3c79820fb3a17ec45265d3a7dd5d
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-16 21:27:30 -0600

    remove unnecessary event listeners in client js, add route to save form data to db

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,12 +4,4 @@ $(document).ready(() => {
   $(`.button-collapse`).sideNav()
   $(`.modal-trigger`).leanModal()
   $(`select`).material_select()
-
-  // event listener for click on tr,
-  // $(`tr`).click(() => {
-  //   const tick_id = $(this).children().text()
-  //
-  //   console.log(tick_id)
-  // })
-
 })
--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,7 @@ router.get(`/`, (req, res, next) => {
   knex(`ticks`)
     .where(`user_id`, 1)
     .then(data => {
-      console.log(data)
-      res.render(`user_home`, {
-        layout: `user_layout.hbs`,
-        data,
-        // name: data.name,
-        // area: data.area,
-        // grade: data.grade,
-        // style: data.style,
-      })
+      res.render(`user_home`, { layout: `user_layout.hbs`, data, })
     })
 })
 
@@ -27,17 +19,36 @@ router.get(`/:id`, (req, res, next) => {
 
 /* POST create new tick at /users/ticks. */
 router.post(`/ticks`, (req, res, next) => {
-  res.render(`index`, { title: `/users/ticks`, })
+  const newTick = req.body
+
+  knex(`ticks`)
+    .insert({
+      user_id: 1,
+      name: newTick.name,
+      area: newTick.area,
+      grade: newTick.grade,
+      style: newTick.style,
+    })
+    .then(() => {
+      knex(`ticks`)
+        .where(`user_id`, 1)
+        .then(data => {
+          res.redirect(`/`)
+        })
+    })
+    .catch(err => {
+      if (err) { return next(err) }
+    })
 })
 
-/* PUT update user at /users/userId. */
-router.put(`/:id`, (req, res, next) => {
-  res.render(`index`, { title: `users/:id`, })
-})
-
-/* DELETE user at /users/userId. */
-router.delete(`/:id`, (req, res, next) => {
-  res.render(`index`, { title: `users/:id`, })
-})
+// /* PUT update user at /users/userId. */
+// router.put(`/:id`, (req, res, next) => {
+//   res.render(`index`, { title: `users/:id`, })
+// })
+//
+// /* DELETE user at /users/userId. */
+// router.delete(`/:id`, (req, res, next) => {
+//   res.render(`index`, { title: `users/:id`, })
+// })
 
 module.exports = router
