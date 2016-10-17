77aed6650c685fdbe60e75c53813ee96ebb23a97
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-16 22:52:01 -0600

    add route for posting new attempt

--- routes/attempts.js
+++ routes/attempts.js
@@ -2,20 +2,42 @@
 
 const express = require(`express`)
 const router = express.Router()
+const knex = require(`../knex.js`)
 
-
-router.get(`/`, (req, res, next) => {
-  res.render(`attempts`, { layout: `user_layout`, })
-})
-
-/* GET read tick and list attempts at /attempts/ticks/tickId. */
+/* GET--read tick and list attempts. */
 router.get(`/ticks/:id`, (req, res, next) => {
-  res.render(`index`, { title: `/users/ticks/:id`, })
+  const tickId = req.params.id
+  let attempts
+
+  knex(`attempts`)
+    .where(`tick_id`, tickId)
+    .then(data => {
+      attempts = data
+      knex(`ticks`)
+        .where(`id`, tickId)
+        .first()
+        .then(tick => {
+          res.render(`attempts`, { layout: `user_layout`, attempts, tick, })
+        })
+    })
 })
 
-/* POST create attempt. */
-router.post(`/`, (req, res, next) => {
-  res.render(`index`, { title: `post: attempts`, })
+/* POST--create attempt. */
+router.post(`/ticks/:id`, (req, res, next) => {
+  const tickId = req.params.id
+  const newAttempt = req.body
+
+  knex(`attempts`)
+    .insert({
+      tick_id: tickId,
+      send_type: newAttempt.send_type,
+      partner: newAttempt.partner,
+      notes: newAttempt.notes,
+    })
+    .then(() => {
+      res.redirect(`/attempts/ticks/${tickId}`)
+    })
+    .catch(err => { if (err) { return next(err) } })
 })
 
 /* DELETE attempt. */
