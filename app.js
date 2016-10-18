a24760593a0ed731f700014148e1cba114f24548
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-18 08:10:57 -0600

    comment out passport w/ facebook

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require(`path`)
 const logger = require(`morgan`)
 const cookieParser = require(`cookie-parser`)
 const bodyParser = require(`body-parser`)
-// const passport = require(`./config/passport.js`)
+const passport = require(`./config/passport.js`)
 
 const routes = require(`./routes/index`)
 const users = require(`./routes/users`)
@@ -24,15 +24,25 @@ app.use(bodyParser.urlencoded({ extended: false, }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, `public`)))
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: `turkey sandwich`,
   resave: false, saveUninitialized: false,
 }))
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(`/`, routes)
 app.use(`/users`, users)
 app.use(`/attempts`, attempts)
+
+// app.get(`/auth/facebook`,
+//   passport.authenticate(`facebook`))
+
+app.get(`/auth/facebook/callback`,
+  passport.authenticate(`facebook`, { failureRedirect: `/`, }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect(`/users`)
+  })
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- config/passport.js
+++ config/passport.js
@@ -1,44 +1,47 @@
-// 'use strict'
+'use strict'
+
+require(`dotenv`).load()
+const app = require(`../app.js`)
+const passport = require(`passport`)
+  // const LocalStrategy = require(`passport-local`).Strategy
+const FacebookStrategy = require(`passport-facebook`).Strategy
+
+// const knex = require()
+
+// passport.use(new LocalStrategy(
+//   (username, password, done) => {
+//     // use knex instaed
+//     User.findOne({ username, }, (err, user) => {
+//       if (err) { return done(err) }
+//       if (!user) {
+//         return done(null, false, { message: `Incorrect username.`, })
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: `Incorrect password.`, })
+//       }
 //
-// require(`dotenv`).load()
-// const passport = require(`passport`)
-//   // const LocalStrategy = require(`passport-local`).Strategy
-// const FacebookStrategy = require(`passport-facebook`).Strategy
-//
-// // const knex = require()
-//
-// // passport.use(new LocalStrategy(
-// //   (username, password, done) => {
-// //     // use knex instaed
-// //     User.findOne({ username, }, (err, user) => {
-// //       if (err) { return done(err) }
-// //       if (!user) {
-// //         return done(null, false, { message: `Incorrect username.`, })
-// //       }
-// //       if (!user.validPassword(password)) {
-// //         return done(null, false, { message: `Incorrect password.`, })
-// //       }
-// //
-// //       return done(null, user)
-// //     })
-// //   }
-// // ))
-//
-// passport.use(new FacebookStrategy({
-//   clientID: process.env.FACEBOOK_APP_ID,
-//   clientSecret: process.env.FACEBOOK_APP_SECRET,
-//   callbackURL: `http://localhost:3000/users/`,
-// }, (accessToken, refreshToken, profile, done) => {
-//   // user.findOrCreate(..., (err, user) => {
-//     // if (err) { return done(err) }
-//     // done(null, user) })
-// }))
-//
-// passport.serializeUser((user, done) => {
-//   done(null, user)
-// })
-// passport.deserializeUser((obj, done) => {
-//   done(null, obj)
-// })
-//
-// module.exports = passport
+//       return done(null, user)
+//     })
+//   }
+// ))
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: `http://localhost:3000/auth/facebook/callback`,
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(`callback`)
+    console.log(profile)
+  // user.findOrCreate(..., (err, user) => {
+    // if (err) { return done(err) }
+    // done(null, user) })
+}))
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
+})
+
+module.exports = passport
