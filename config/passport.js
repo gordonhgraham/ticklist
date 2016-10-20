cb0fd17499e26f646c805997649757190ea298cb
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-20 10:57:16 -0600

    fix migration file to make fb_id unique, extract email value from email object of facebook.profile

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,14 @@
 'use strict'
 
+/* eslint-disable camelcase */
+
 require(`dotenv`).load()
 
 // const app = require(`../app.js`)
 // const LocalStrategy = require(`passport-local`).Strategy
 const passport = require(`passport`)
 const FacebookStrategy = require(`passport-facebook`).Strategy
-
-// const knex = require(`../knex.js`)
-
-// const knex = require()
+const knex = require(`../knex.js`)
 
 // passport.use(new LocalStrategy(
 //   (username, password, done) => {
@@ -35,9 +34,29 @@ passport.use(new FacebookStrategy({
   profileFields: [`id`, `email`, `first_name`, `last_name`]
 },
   (accessToken, refreshToken, profile, cb) => {
-    console.log(`accessToken`, accessToken)
-    console.log(`refreshToken`, refreshToken)
-    console.log(`profile`, profile)
+    knex(`users`)
+      .where(`fb_id`, profile.id)
+      .first()
+      .then(data => {
+        // if user exists, redirect to user homepage
+        if (data) {
+          // update user
+        } else {
+          // if user doesn't exist insert user
+          knex(`users`)
+            .insert({
+              fb_id: profile.id,
+              email: profile.emails[0].value,
+              first_name: profile.name.givenName,
+              last_name: profile.name.familyName,
+              token: accessToken
+            })
+            .then((err, user) => {
+              console.log(`user--`, user)
+              return cb(err, user)
+            })
+        }
+      })
 
     // User.findOrCreate({ facebookId: profile.id }, (err, user) => {
     //   return cb(err, user)
--- migrations/20161016124621_users.js
+++ migrations/20161016124621_users.js
@@ -4,7 +4,7 @@ exports.up = knex => {
   return knex.schema.dropTableIfExists(`users`).then(() => {
     return knex.schema.createTable(`users`, table => {
       table.increments()
-      table.integer(`fb_id`).notNullable()
+      table.string(`fb_id`).notNullable().unique()
       table.string(`email`).unique()
       table.string(`token`).notNullable()
       table.string(`first_name`)
