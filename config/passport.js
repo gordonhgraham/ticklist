850a55c87f03aee40532685abd183887910c8848
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-20 20:19:50 -0600

    change APP_ID to CLIENT_ID in .env

--- config/passport.js
+++ config/passport.js
@@ -35,8 +35,8 @@ passport.deserializeUser((obj, done) => {
 })
 
 passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   callbackURL: `http://localhost:3000/auth/facebook/callback`,
   profileFields: [`id`, `email`, `first_name`, `last_name`]
 },
