ee0f9cd981cf19268d943d99dbad8625f573f593
Gordon H Graham <theboss@gordonhgraham.com>
2017-02-11 17:56:04 -0700

    lint index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 'use strict'
 
 const express = require(`express`)
-const passport = require(`../config/passport`)
 const router = express.Router()
 
 /* GET landing page. */
-router.get(`/`, (req, res, next) => {
-  res.render(`index`, { title: `index`, })
+router.get(`/`, (req, res) => {
+  res.render(`index`, { title: `index` })
 })
 
 /* GET user logout. */
-router.get(`/logout`, (req, res, next) => {
+router.get(`/logout`, (req, res) => {
   req.logout()
-  console.log(`req.session`, req.session)
-  console.log(`req.user`, req.user)
   res.redirect(`/`)
 })
 
